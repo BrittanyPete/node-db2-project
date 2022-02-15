17b8e3e1a6bcdf3f3f1a9463dbdf8cfee1226120
Brittany Petersen <britt.petersen100@gmail.com>
2022-02-15 14:32:48 -0700

    added migrations, cars db, and middleware

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,64 @@
+const Cars = require('./cars-model');
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique
+}
+
+
 const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
+  try {
+    const carId = await Cars.getById(req.params.id);
+    if (!carId) {
+      res.status(404).json({
+        message: `car with id ${carId} is not found`
+      })
+    }
+  }
+  catch (err) {
+    next(err)
+  }
 }
 
 const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+  const { vin, make, model, mileage } = req.body;
+  if(!vin) {
+    res.status(400).json({ message: 'vin is missing'})
+  } else if (!make) {
+    res.status(400).json({ message: 'make is missing'})
+  } else if (!model) {
+    res.status(400).json({ message: 'model is missing'})
+  } else if (!mileage) {
+    res.status(400).json({ message: 'mileage is missing'})
+  }
 }
 
 const checkVinNumberValid = (req, res, next) => {
-  // DO YOUR MAGIC
+  try {
+    const {vin} = req.body.vin
+    if (vin.length !== 17) {
+      res.status(400).json({ message: `vin ${vin} is invalid`})
+    }
+  }
+  catch (err) {
+    next(err)
+  }
 }
 
-const checkVinNumberUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+const checkVinNumberUnique = async (req, res, next) => {
+  const { vin } = req.body.vin;
+  try {
+    const exists = await db('cars')
+    .where('vin', vin.trim())
+    .first()
+
+    if(exists) {
+      res.status(400).json({ message: `vin ${vin} already exists`})
+    }
+  }
+  catch (err) {
+    next(err)
+  }
 }
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,11 @@
 // STRETCH
+exports.seed = async function(knex) {
+    await knex('cars').truncate()
+    return knex('cars').insert([
+        {vin: '1234567890abcdfeg', make: 'Honda', model: 'Accord', mileage: 297100, title: 'clean', transmission: 'manual'},
+        {vin: '1235567890abcdfeg', make: 'Ford', model: 'Focus', mileage: 97000, title: 'salvaged', transmission: 'automatic'},
+        {vin: '1236567890abcdfeg', make: 'Jeep', model: 'Cherokee', mileage: 76012, title: 'clean', transmission: 'automatic'},
+        {vin: '1237567890abcdfeg', make: 'Toyota', model: 'Tacoma', mileage: 23852, title: 'clean', transmission: 'manual'},
+        {vin: '1238567890abcdfeg', make: 'Nissan', model: 'Altima', mileage: 136442, title: 'salvaged', transmission: 'automatic'},
+    ])
+}
